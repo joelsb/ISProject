9259d92d34eeb35bfa42056e11fcf417b44e8096
joelsb9 <2191618@my.ipleiria.pt>
2023-12-20 14:43:59 +0000

    First Commit

--- Somiod/Controllers/SomiodController.cs
+++ Somiod/Controllers/SomiodController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,6 +10,23 @@ namespace Somiod.Controllers
 {
     public class SomiodController : ApiController
     {
+        private static string strDataConn;
+        public SomiodController()
+        {
+            // Dynamically calculate the project path
+            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+            string projectPath = Path.Combine(executablePath, @"..\..");
+
+            // Set DataDirectory to the project path
+            AppDomain.CurrentDomain.SetData("DataDirectory", projectPath);
+
+            // Use |DataDirectory| in the connection string
+            string relativePath = @"|DataDirectory|\App_Data\DBProds.mdf";
+            strDataConn = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={relativePath};Integrated Security=True";
+        }
+
+        //String strDataConn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\EscolaJoel\ESTG5Ano\1Semestre\IS\Pratica\VisualStudio2023\3.ProductsAPI\ProductsAPI\App_Data\DBProds.mdf;Integrated Security = True";
+
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
