fcfb5eca5cf13561f36c11cc9a455cba97c00957
Tiago Baptista <2191826@my.ipleiria.pt>
2023-12-22 22:38:15 +0000

    App client A incompleta

---
+++ App_clienntA/ClientA.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace App_clienntA
+{
+    partial class ClientA
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBoxShowApp = new System.Windows.Forms.RichTextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.richTextShowContainer = new System.Windows.Forms.RichTextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "POSTNewApp";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.POSTNewApp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(84, 84);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "UPDATEApp";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.UPDATEApp);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(191, 84);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(197, 22);
+            this.textBoxOutput.TabIndex = 4;
+            this.textBoxOutput.Text = "textBoxOutput";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 44);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "POSTNewContainer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.POSTNewContainer);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "POSTNewSubcription";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.POSTNewSubcription);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(161, 137);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 43);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "UPDATEContainer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.UPDATEContainer);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(138, 33);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 44);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "DELApp";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.DELApp);
+            // 
+            // richTextBoxShowApp
+            // 
+            this.richTextBoxShowApp.Location = new System.Drawing.Point(524, 54);
+            this.richTextBoxShowApp.Name = "richTextBoxShowApp";
+            this.richTextBoxShowApp.Size = new System.Drawing.Size(279, 153);
+            this.richTextBoxShowApp.TabIndex = 9;
+            this.richTextBoxShowApp.Text = "richTextBoxShowApp";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(660, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(132, 43);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "GETAllApp";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.GETAllApp);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(305, 137);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 43);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "DELContainer";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.DELContainer);
+            // 
+            // richTextShowContainer
+            // 
+            this.richTextShowContainer.Location = new System.Drawing.Point(526, 269);
+            this.richTextShowContainer.Name = "richTextShowContainer";
+            this.richTextShowContainer.Size = new System.Drawing.Size(272, 178);
+            this.richTextShowContainer.TabIndex = 12;
+            this.richTextShowContainer.Text = "richTextShowContainer";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(675, 222);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(122, 39);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "GETAllContainer";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.GETAllContainer);
+            // 
+            // ClientA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.richTextShowContainer);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.richTextBoxShowApp);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "ClientA";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ClientA_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RichTextBox richTextBoxShowApp;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.RichTextBox richTextShowContainer;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ App_clienntA/ClientA.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO; //Stream
+using System.Linq;
+using System.Net; //HttpWebRequest
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Xml;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Application = Somiod.Models.Application;
+using Container = Somiod.Models.Container;
+using Subscription = Somiod.Models.Subscription;
+
+namespace App_clienntA
+{
+    public partial class ClientA : Form
+    {
+        string baseURL = @"http://localhost:50611/api/somiod";
+
+        List<Application> applications = new List<Application>();
+        Application app;
+
+        List<Container> containers = new List<Container>();
+        Container container;
+
+        List<Subscription> subscriptions = new List<Subscription>();
+        Subscription subscription;
+
+        MqttClient mClient = new MqttClient(IPAddress.Parse("127.0.0.1")); //OR use the broker hostname
+        String[] mStrSubscription = { "TEST" }; //Subscription
+
+
+        public ClientA()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ClientA_Load(object sender, EventArgs e)
+        {
+
+            mClient.Connect(Guid.NewGuid().ToString());
+            if (!mClient.IsConnected)
+            {
+                MessageBox.Show("Error connecting to broker...");
+                return;
+            }
+
+            //This client's subscription operation id done 
+            mClient.MqttMsgSubscribed += null;
+
+
+            //Subscribe 
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE };//QoS
+            mClient.Subscribe(mStrSubscription, qosLevels);
+        }
+
+        private void POSTNewApp(object sender, EventArgs e)
+        {
+            Application app = new Application();
+            app.Id = -1;
+            app.Name = textBoxName.Text;
+            app.CreationDt = DateTime.Now;
+
+
+            var bodyStream = new StreamReader(HttpContext.Current.Request.InputStream);
+            string xmlContract = bodyStream.ReadToEnd();
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContract);
+
+            POSTandPUT("POST", baseURL, xmlContract);
+
+            textBoxOutput.Text = "xnl contract: " + xmlContract + Environment.NewLine;
+            /*
+            System.Text.UTF8Encoding encoding = new UTF8Encoding();
+            byte[] byteArray = encoding.GetBytes(xml);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseURI);
+            request.Method = "post"; //add new
+            request.ContentLength = byteArray.Length;
+            request.ContentType = @"application/xnl";
+
+            Stream data = request.GetRequestStream();
+            data.Write(byteArray, 0, byteArray.Length);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            MessageBox.Show(response.ContentLength.ToString());
+            response.Close();*/
+
+        }
+
+        private void UPDATEApp(object sender, EventArgs e)
+        {
+            Application app = new Application();
+            textBoxName.Text = app.Name;
+            app.CreationDt = DateTime.Now;
+
+
+            var bodyStream = new StreamReader(HttpContext.Current.Request.InputStream);
+            string xmlContract = bodyStream.ReadToEnd();
+
+
+
+            POSTandPUT("PUT", baseURL + $"/{app.Name}", xmlContract);
+
+            textBoxOutput.Text = "xnl contract: " + xmlContract + Environment.NewLine;
+
+        }
+        private void DELApp(object sender, EventArgs e)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(app.Name);
+            request.Method = "delete";
+            request.ContentType = @"application/xml";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            MessageBox.Show(response.StatusCode.ToString());
+            response.Close();
+        }
+
+        static string ShowApplicationInfo(Application app)
+        {
+            return string.Format("{0} : \t{1} \t{2} \t{3}", app.Id, app.Name, app.CreationDt);
+        }
+
+
+        private void GETAllApp(object sender, EventArgs e)
+        {
+            string content = Get(baseURL);
+            richTextBoxShowApp.Text = "";
+
+            applications = applications;
+            foreach (Application item in applications)
+            {
+                richTextBoxShowApp.AppendText(ShowApplicationInfo(item) + Environment.NewLine);
+            }
+
+        }
+
+        private void POSTNewContainer(object sender, EventArgs e)
+        {
+
+            Container container = new Container();
+            container.Id = -1;
+            container.Name = textBoxName.Text;
+            container.CreationDt = DateTime.Now;
+            container.Parent = app.Id;
+
+
+
+            var bodyStream = new StreamReader(HttpContext.Current.Request.InputStream);
+            string xmlContract = bodyStream.ReadToEnd();
+
+
+            POSTandPUT("POST", baseURL, xmlContract);
+
+            textBoxOutput.Text = "xnl contract: " + xmlContract + Environment.NewLine;
+        }
+
+        private void UPDATEContainer(object sender, EventArgs e)
+        {
+            Container container = new Container();
+            container.Id = -1;
+            textBoxName.Text = container.Name;
+            container.CreationDt = DateTime.Now;
+            app.Id = container.Parent;
+
+            var bodyStream = new StreamReader(HttpContext.Current.Request.InputStream);
+            string xmlContract = bodyStream.ReadToEnd();
+
+            POSTandPUT("PUT", baseURL + $"/{container.Id}", xmlContract);
+            textBoxOutput.Text = "xnl contract: " + xmlContract + Environment.NewLine;
+        }
+
+        private void DELContainer(object sender, EventArgs e)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(container.Name);
+            request.Method = "delete";
+            request.ContentType = @"application/xml";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            MessageBox.Show(response.StatusCode.ToString());
+            response.Close();
+        }
+
+        static string ShowContainernInfo(Container container)
+        {
+            return string.Format("{0} : \t{1} \t{2} \t{3} \t{4}", container.Id, container.Name, container.CreationDt,container.Parent);
+        }
+
+        private void GETAllContainer(object sender, EventArgs e)
+        {
+            string content = Get(baseURL);
+            richTextShowContainer.Text = "";
+
+            //Container = Containers;
+            foreach (Container item in containers)
+            {
+                richTextShowContainer.AppendText(ShowContainernInfo(item) + Environment.NewLine);
+            }
+
+        }
+
+
+        private void POSTNewSubcription(object sender, EventArgs e)
+        {
+
+            Subscription subscription = new Subscription();
+            subscription.Id = -1;
+            subscription.Name = textBoxName.Text;
+            subscription.CreationDt = DateTime.Now;
+            subscription.Parent = container.Id;
+
+
+
+            var bodyStream = new StreamReader(HttpContext.Current.Request.InputStream);
+            string xmlContract = bodyStream.ReadToEnd();
+
+
+            POSTandPUT("POST", baseURL, xmlContract);
+
+            textBoxOutput.Text = "xnl contract: " + xmlContract + Environment.NewLine;
+
+        }
+
+
+        private void POSTandPUT(string method, string url, string xmlContract)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method; //"POST" or "PUT"
+
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            Byte[] byteArray = encoding.GetBytes(xmlContract);
+
+            request.ContentLength = byteArray.Length;
+            request.ContentType = @"application/xml";
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+            long length = 0;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    length = response.ContentLength;
+                    MessageBox.Show(length.ToString());
+                }
+            }
+            catch (WebException ex)
+            {
+                // Log exception and throw as for GET example above
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private string Get(string URL)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string content = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            return content;
+        }
+
+    }
+}
